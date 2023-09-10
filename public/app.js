b8aaaaff2ac584a138c16699f5a772e60fb525f9
Iamneo <techadmin@iamneo.ai>
2023-09-10 04:56:06 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -251,14 +251,14 @@ function display() {
   }
   
   // Function to reset game
-  function myfunc_2() {
+  function reset() {
     location.reload();
     b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = '';
   }
   
   // Checking for whose turn it is to play
   flag = 1;
-  function myfunc_3() {
+  function check_turn1() {
     if (flag == 1) {
         document.getElementById("b1").value = "X";
         document.getElementById("b1").disabled = true;
@@ -271,7 +271,7 @@ function display() {
     }
   }
   
-  function myfunc_4() {
+  function check_turn2() {
     if (flag == 1) {
         document.getElementById("b2").value = "X";
         document.getElementById("b2").disabled = true;
@@ -284,7 +284,7 @@ function display() {
     }
   }
   
-  function myfunc_5() {
+  function check_turn3() {
     if (flag == 1) {
         document.getElementById("b3").value = "X";
         document.getElementById("b3").disabled = true;
@@ -297,7 +297,7 @@ function display() {
     }
   }
   
-  function myfunc_6() {
+  function check_turn4() {
     if (flag == 1) {
         document.getElementById("b4").value = "X";
         document.getElementById("b4").disabled = true;
@@ -310,7 +310,7 @@ function display() {
     }
   }
   
-  function myfunc_7() {
+  function check_turn5() {
     if (flag == 1) {
         document.getElementById("b5").value = "X";
         document.getElementById("b5").disabled = true;
@@ -323,7 +323,7 @@ function display() {
     }
   }
   
-  function myfunc_8() {
+  function check_turn6() {
     if (flag == 1) {
         document.getElementById("b6").value = "X";
         document.getElementById("b6").disabled = true;
@@ -336,7 +336,7 @@ function display() {
     }
   }
   
-  function myfunc_9() {
+  function check_turn7() {
     if (flag == 1) {
         document.getElementById("b7").value = "X";
         document.getElementById("b7").disabled = true;
@@ -349,7 +349,7 @@ function display() {
     }
   }
   
-  function myfunc_10() {
+  function check_turn8() {
     if (flag == 1) {
         document.getElementById("b8").value = "X";
         document.getElementById("b8").disabled = true;
@@ -362,7 +362,7 @@ function display() {
     }
   }
   
-  function myfunc_11() {
+  function check_turn9() {
     if (flag == 1) {
         document.getElementById("b9").value = "X";
         document.getElementById("b9").disabled = true;
